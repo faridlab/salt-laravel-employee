046d19024c93a6651994e781fbf38cc17a7f6af3
Farid Hidayat <farid.hidayat@sagara.id>
2022-07-26 21:56:14 +0700

    feat: Employee Contacts model file

---
+++ src/Models/EmployeeContacts.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace SaltEmployee\Models;
+
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Database\Eloquent\Model;
+use DB;
+use Illuminate\Support\Facades\Schema;
+
+use SaltLaravel\Models\Resources;
+use SaltLaravel\Traits\ObservableModel;
+use SaltLaravel\Traits\Uuids;
+
+class EmployeeContacts extends Resources {
+
+    use Uuids;
+    use ObservableModel;
+
+    protected $filters = [
+        'default',
+        'search',
+        'fields',
+        'relationship',
+        'withtrashed',
+        'orderby',
+
+        // Fields table
+        'id',
+        'employee_id',
+        'name',
+        'relationship',
+        'phone',
+        'email',
+    ];
+
+
+    protected $rules = array(
+        'employee_id' => 'required|string',
+        'name' => 'required|string',
+        'relationship' => 'required|string',
+        'phone' => 'required|string',
+        'email' => 'nullable|string',
+    );
+
+    protected $auths = array (
+        'index',
+        'store',
+        'show',
+        'update',
+        'patch',
+        'destroy',
+        'trash',
+        'trashed',
+        'restore',
+        'delete',
+        'import',
+        'export',
+        'report'
+    );
+
+    protected $structures = array();
+    protected $forms = array();
+
+    protected $searchable = array(
+        'employee_id',
+        'name',
+        'relationship',
+        'phone',
+        'email',
+    );
+
+}
