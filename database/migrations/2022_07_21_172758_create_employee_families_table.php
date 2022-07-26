5f0f4619ce0118e1bf3900a0df9d0820455f2787
Farid Hidayat <farid.hidayat@sagara.id>
2022-07-26 21:49:18 +0700

    feat: employee families migration file

---
+++ database/migrations/2022_07_21_172758_create_employee_families_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employee_families', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->foreignUuid('employee_id')->references('id')->on('employees');
+
+            $table->string('name');
+            $table->enum('relationship', [
+                'father',
+                'mother',
+                'sibling',
+                'spouse',
+                'child',
+                'cousin',
+                'nibling',
+                'parent-in-law',
+                'brother-in-law',
+                'sister-in-law',
+                'uncle',
+                'aunt',
+            ]);
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employee_families');
+    }
+};
